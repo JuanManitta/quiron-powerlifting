0bc33ace2dfe4b5d7e2e70bf783c7a083f0e3016
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-10-01 09:29:50 +0200

    updt

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ Crear cuenta </Button>
   return (
     <ThemeProvider>
       <NavigationMenu>
-        <NavigationMenuList className="p-4">
+        <NavigationMenuList className="p-4 flex items-center">
           { routes.map((route, index ) => (
               <NavigationMenuItem 
                 key={index} 
@@ -59,8 +59,8 @@ Crear cuenta </Button>
           <div className="grid grid-cols-2 h-screen justify-center items-center m-auto max-w-7xl ">
             <div className="col-span-2 lg:col-span-1 flex justify-center lg:justify-end">
               <div className="max-w-2xl">
-                <h1 className="text-6xl text-center font-bold mb-4 ">DASH Powerlifting</h1>
-                <p className="text-left text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ratione sapiente ea hic incidunt dolor aliquam porro vero obcaecati repudiandae. Recusandae dolores sunt voluptatibus numquam dolorem facilis eum unde impedit.</p>
+                <h1 className="text-4xl md:text-6xl text-left md:text-center font-bold mb-4 ">DASH Powerlifting</h1>
+                <p className="text-left text-md md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ratione sapiente ea hic incidunt dolor aliquam porro vero obcaecati repudiandae. Recusandae dolores sunt voluptatibus numquam dolorem facilis eum unde impedit.</p>
                 <div className="py-12">
                   <Button size='lg' className="text-lg mt-4">
                     Comenzar
@@ -76,23 +76,23 @@ Crear cuenta </Button>
         </section>
 
         <section>
-          <h2 className="text-3xl text-center pb-12 p-6">
+          <h2 className="text-xl mg:text-3xl text-left md:text-center pb-12 p-6">
             Entrenadores de todo el mundo ya estan usando DASH Powerlifting
           </h2>
           <div className="bg-primary">
             <div className="m-auto max-w-7xl ">
               <div className="p-4  flex justify-around">
                 <div className="flex flex-col">
-                  <span className="text-6xl m-auto">40+</span>
-                  <span className="text-2xl">Entrenadores</span>
+                  <span className="text-3xl md:text-6xl m-auto">40+</span>
+                  <span className="text-lg md:text-2xl">Entrenadores</span>
                 </div>
                 <div className="flex flex-col">
-                  <span className="text-6xl m-auto">40+</span>
-                  <span className="text-2xl">Entrenadores</span>
+                  <span className="text-3xl md:text-6xl m-auto">40+</span>
+                  <span className="text-lg md:text-2xl">Entrenadores</span>
                 </div>
                 <div className="flex flex-col">
-                  <span className="text-6xl m-auto">40+</span>
-                  <span className="text-2xl">Entrenadores</span>
+                  <span className="text-3xl md:text-6xl m-auto">40+</span>
+                  <span className="text-lg md:text-2xl">Entrenadores</span>
                 </div>
               </div>
             </div>
@@ -103,23 +103,23 @@ Crear cuenta </Button>
 
           <div className="grid grid-cols-2 justify-center py-16 m-auto max-w-7xl">
             <div className="col-span-2">
-              <h3 className="text-center text-6xl py-16 font-bold">¿Cómo funciona?</h3>
+              <h3 className="text-center text-4xl md:text-6xl py-16 font-bold">¿Cómo funciona?</h3>
             </div>
 
             <div className="col-span-2 max-w-5xl mx-auto py-16">
-              <p className="text-center text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vitae impedit reprehenderit aliquid ullam, magni fugit deleniti amet dignissimos et eaque rem dolorum natus quia earum aliquam accusamus voluptas nemo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsa, totam amet aliquid necessitatibus porro quasi sequi nulla eum quia cum sapiente rerum iste veniam, minus perspiciatis fugit? Illum, distinctio.</p>
+              <p className="text-center text-md md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vitae impedit reprehenderit aliquid ullam, magni fugit deleniti amet dignissimos et eaque rem dolorum natus quia earum aliquam accusamus voluptas nemo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsa, totam amet aliquid necessitatibus porro quasi sequi nulla eum quia cum sapiente rerum iste veniam, minus perspiciatis fugit? Illum, distinctio.</p>
             </div>
 
-            <div className="col-span-1 py-12">
+            <div className="col-span-2 md:col-span-1 py-12">
               <img src={ background1 }  alt="" />
             </div>
 
-            <div className="col-span-1 py-12">
+            <div className="col-span-2 md:col-span-1 py-12">
               <img src={ background1 }  alt="" />
             </div>
 
             <div className="col-span-2 py-6">
-              <Button className="ml-12" size='lg' variant='secondary'>
+              <Button className="ml-12 text-lg" size='lg' variant='secondary'>
                 Comenzar
               </Button>
             </div>
@@ -130,7 +130,7 @@ Crear cuenta </Button>
         <section className="p-6">
           <div className="grid grid-cols-2 py-16 gap-12 m-auto max-w-7xl">
             <div className="col-span-2">
-              <h4 className="text-5xl text-center mb-4 py-16 font-bold">Estadísticas en tiempo real</h4>
+              <h4 className="text-4xl md:text-5xl text-center mb-4 py-16 font-bold">Estadísticas en tiempo real</h4>
             </div>
 
             <div className="col-span-2 lg:col-span-1">
@@ -138,9 +138,9 @@ Crear cuenta </Button>
             </div>
 
             <div className="col-span-2 lg:col-span-1 py-16">
-              <p className="text-xl"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae aperiam omnis odio officia reiciendis repudiandae fugiat accusantium cumque porro explicabo, alias obcaecati corporis ipsam, sapiente distinctio quo accusamus ipsum debitis!</p>
+              <p className="text-md md:text-lg"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae aperiam omnis odio officia reiciendis repudiandae fugiat accusantium cumque porro explicabo, alias obcaecati corporis ipsam, sapiente distinctio quo accusamus ipsum debitis!</p>
               <div className="py-12">
-                <Button size='lg' >
+                <Button size='lg' className="text-lg" >
                   Comenzar
                 </Button>
               </div>
